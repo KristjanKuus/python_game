24c7d3914ea177482665ae441ee811e9f8d2c21c
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-04-06 10:58:07 +0300

    Displays Troll on screen

--- game.py
+++ game.py
@@ -1,19 +1,22 @@
 import sys
 import pygame
 from settings import Settings
+from troll import Troll
 
 def run_game():
     pygame.init()
     game_settings = Settings()
     screen = pygame.display.set_mode((game_settings.screen_width,game_settings.screen_height))
     pygame.display.set_caption("Example Game")
-    bg_color = (18, 5, 255)
+
+    troll = Troll(screen)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
         screen.fill(game_settings.bg_color)
+        troll.blitme()
         pygame.display.flip()
 
 run_game()
