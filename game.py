dbf87efb4625adc3d9413663a5d8f7d0953f562f
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-04-06 10:38:57 +0300

    Defined background color

--- game.py
+++ game.py
@@ -5,11 +5,13 @@ def run_game():
     pygame.init()
     screen = pygame.display.set_mode((800,600))
     pygame.display.set_caption("Example Game")
+    bg_color = (18, 5, 255)
 
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 sys.exit()
+        screen.fill(bg_color)
         pygame.display.flip()
 
 run_game()
