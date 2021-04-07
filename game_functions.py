7172a8c10765315722a18299a50d367209b953d0
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-04-07 14:20:49 +0300

    Refactored keyboard event logic

--- game_functions.py
+++ game_functions.py
@@ -1,19 +1,25 @@
 import sys
 import pygame
+def check_keydown_events(event, troll):
+    if event.key == pygame.K_RIGHT:
+        troll.moving_right = True
+    if event.key == pygame.K_LEFT:
+        troll.moving_left = True
+
+def check_keyup_events(event, troll):
+    if event.key == pygame.K_RIGHT:
+        troll.moving_right = False
+    if event.key == pygame.K_LEFT:
+        troll.moving_left = False
+
 def check_events(troll):
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             sys.exit()
         elif event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_RIGHT:
-                troll.moving_right = True
-            if event.key == pygame.K_LEFT:
-                troll.moving_left = True
+            check_keydown_events(event, troll)
         elif event.type == pygame.KEYUP:
-            if event.key == pygame.K_RIGHT:
-                troll.moving_right = False
-            if event.key == pygame.K_LEFT:
-                troll.moving_left = False
+            check_keyup_events(event, troll)
 
 def update_screen(game_settings, screen, troll):
     screen.fill(game_settings.bg_color)
--- settings.py
+++ settings.py
@@ -5,4 +5,4 @@ class Settings:
         self.screen_width = 1920
         self.screen_height = 600
         self.bg_color = (18, 5, 255)
-        self.troll_speed_factor = 0.5
+        self.troll_speed_factor = 1.5
