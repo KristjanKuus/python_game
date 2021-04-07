1ed33cc4d816e92fdaacbb1435bba927ffb058fd
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-04-07 14:07:27 +0300

    Now the character can move left.

--- game_functions.py
+++ game_functions.py
@@ -7,9 +7,13 @@ def check_events(troll):
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
                 troll.moving_right = True
+            if event.key == pygame.K_LEFT:
+                troll.moving_left = True
         elif event.type == pygame.KEYUP:
             if event.key == pygame.K_RIGHT:
                 troll.moving_right = False
+            if event.key == pygame.K_LEFT:
+                troll.moving_left = False
 
 def update_screen(game_settings, screen, troll):
     screen.fill(game_settings.bg_color)
