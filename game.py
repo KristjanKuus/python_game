f808489443cd118ec4181c1a14f52a557b7cd556
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-04-07 14:04:26 +0300

    Now the character responds to holding down the right arrow button.

--- game.py
+++ game.py
@@ -8,12 +8,13 @@ def run_game():
     pygame.init()
     game_settings = Settings()
     screen = pygame.display.set_mode((game_settings.screen_width,game_settings.screen_height))
-    pygame.display.set_caption("Example Game")
+    pygame.display.set_caption("U Mad Bro?")
 
     troll = Troll(screen)
 
     while True:
         gf.check_events(troll)
+        troll.update()
         gf.update_screen(game_settings, screen, troll)
 
 run_game()
--- game_functions.py
+++ game_functions.py
@@ -6,7 +6,10 @@ def check_events(troll):
             sys.exit()
         elif event.type == pygame.KEYDOWN:
             if event.key == pygame.K_RIGHT:
-                troll.rect.centerx += 1
+                troll.moving_right = True
+        elif event.type == pygame.KEYUP:
+            if event.key == pygame.K_RIGHT:
+                troll.moving_right = False
 
 def update_screen(game_settings, screen, troll):
     screen.fill(game_settings.bg_color)
