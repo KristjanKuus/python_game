88862f771068b1847a1fae650cee7c04c34bb4c1
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-04-08 11:42:11 +0300

    Deletes old bullets

--- game.py
+++ game.py
@@ -18,6 +18,9 @@ def run_game():
         gf.check_events(game_settings, screen, troll, bullets)
         troll.update()
         bullets.update()
+        for bullet in bullets.copy():
+            if bullet.rect.bottom <= 0:
+                bullets.remove(bullet)
         gf.update_screen(game_settings, screen, troll, bullets)
 
 run_game()
