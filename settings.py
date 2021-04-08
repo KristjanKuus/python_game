7b1b577ec72d7d182eb903294c4a67764121ea3f
Kristjan Kuus <kristjan.kuus.001@khk.ee>
2021-04-08 11:26:19 +0300

    Created bullet configuration and description.

--- settings.py
+++ settings.py
@@ -6,3 +6,7 @@ class Settings:
         self.screen_height = 600
         self.bg_color = (18, 5, 255)
         self.troll_speed_factor = 1.5
+        self.bullet_speed_factor = 0.5
+        self.bullet_width = 3
+        self.bullet_height = 15
+        self.bullet_color = 255, 255, 0
